b80639fe48f97d7a0f4a7a6c7542fdb61f459efc
Nico Gago.- <nicogago.-@hotmail.com>
2015-04-22 22:32:58 -0300

    mejoras en start y stop

---
+++ FUNCIONES/glog.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#Glog
+#Parametro1 (obligatorio):comando (nombre del comando o funcion que genera el mensaje)
+#Parametro2 (obligatorio): mensaje
+#Parametro3 (opcional):tipo de mensaje [INFO,WAR,ERR]
+
+#FALTA HACER QUE SI ES DEL INSTALADOR LO LOGUEA EN OTRO LADO (PONERLE LOS DIRECTORIOS CDO ESTE TERMINADO EL INSTALADOR)
+
+#VALIDAMOS QUE EL SCRIPT RECIBA 2 O 3 PARAMETROS
+log="InsPro.log"
+
+if [ $# -gt 3 ] || [ $# -lt 2 ]; then
+	echo "No se pudo loguear mensaje, cantidad de parametros incorrecta: $# " >> log
+	exit
+fi
+
+#VALIDAMOS QUE EL TERCER PARAMETRO SEA DEL TIPO INFO,WAR,ERR
+if [ ! $3 ]; then # no existe el tercer parametro
+	echo "Tipo de mensaje no ingresado: Se toma el valor por defautl [INFO]" >> log
+	tipoMensaje="INFO"
+else # existe el tercer parametro
+	if [ $3 = "INFO" ] || [ $3 = "WAR" ] || [ $3 = "ERR" ]; then
+		tipoMensaje=$3
+	else
+		echo "Tipo de mensaje invalido: Se toma el valor por defautl [INFO]" >> log
+		tipoMensaje="INFO"	
+	fi
+fi
+
+#TODO FALTA HACER QUE NO SEA UN LOG INFINITO
+
+#Escribo el archivo
+fecha=`date +"%X %x"`
+comando=$1
+mensaje=$2
+if [ $1 = "MOVER" ] || [ $1 = "START" ]; then
+echo $fecha $USER $comando $mensaje $tipoMensaje >> LOGDIR/funciones
+#TODO terminar para el resto de las carpetas
+fi
--- FUNCIONES/stop.sh
+++ FUNCIONES/stop.sh
@@ -5,10 +5,13 @@
 # demonio a detener
 
 #Debe ser un solo parametro
-if [ $# -gt 2 -o $# -lt 1 ]; then
+if [ $# -gt 1 -o $# -lt 1 ]; then
 	echo "Cantidad de parametros inválida"
 	exit 1
 fi
+
+#TODO como garantizo que se pueda correr por consola? lo tendria que realizar la instalacion ?
+#TODO debe chequear que este corriendo el demonio
 
 #mato al demonio
 killall "$1"
