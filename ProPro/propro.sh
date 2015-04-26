1a844e658c5086b3f76da115f6af2034ee3c93f8
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-04-26 20:19:50 -0300

    proceso los archivos provenientes del ACEPDIR cronologicamente

--- ProPro/propro.sh
+++ ProPro/propro.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-input="/ACEPDIR/Alfonsin/"
+input=/ACEPDIR/Alfonsin/*
 countFiles="$(find ./ACEPDIR/Alfonsin -type f -printf x | wc -c)"
 
- sh glog.sh PROPRO "inicio de PROPRO \n \t\t\t Cantidad de archivos a procesar: <$countFiles>" INFO
+ sh glog.sh PROPRO "Inicio de propro \n \t\t\t Cantidad de archivos a procesar: <$countFiles>" INFO
+
+ for filename in `ls ./ACEPDIR/Alfonsin/ | cut -d '_' -f 5 | sort -t - -k 3 -k 2 -k 1`; 
+ do 
+ 	echo $filename; 
+ done;
