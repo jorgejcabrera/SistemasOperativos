1210dddbf2579261b81e34b97f276cd05abcc94f
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-04-27 20:57:29 -0300

    muevo el archivo despues de protocolizarlo

--- ProPro/propro.sh
+++ ProPro/propro.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-countFiles="$(find ./ACEPDIR/Alfonsin -type f -printf x | wc -c)"
+codeGestion="Alfonsin"
+countFiles="$(find ./ACEPDIR/$codeGestion -type f -printf x | wc -c)"
 sh glog.sh PROPRO "Inicio de propro \n \t\t\t Cantidad de archivos a procesar: $countFiles" INFO
-codGestion="Alfonsin"
-for filename in `ls ./ACEPDIR/Alfonsin/ | cut -d '_' -f 5 | sort -t - -k 3 -k 2 -k 1`; 
+for completeFileName in `ls ./ACEPDIR/$codeGestion/ | cut -d '_' -f 5 | sort -t - -k 3 -k 2 -k 1`; 
 do 
 	#protocolizar cada archivo
- 	fileName=$(find ./ACEPDIR/$codGestion -type f -name "*$filename")
- 	anioGestion=$(echo $fileName | cut -d '-' -f 3 | cut -d '.' -f 1)
- 	codigoNorma=$(echo $fileName | cut -d '_' -f 2)
- 	echo $codigoNorma
- 	currentFileName=$( echo $fileName | cut -d '/' -f 4 )
- 	sh glog.sh PROPRO "Archivo a procesar $currentFileName" INFO
+ 	completeFileName=$(find ./ACEPDIR/$codeGestion -type f -name "*$completeFileName")
+ 	yearNorm=$(echo $completeFileName | cut -d '-' -f 3 | cut -d '.' -f 1)
+ 	codeNorm=$(echo $completeFileName | cut -d '_' -f 2)
+ 	fileDocketedName="$yearNorm.$codeNorm"
+ 	fileAlreadyDocketed=$(find ./PROCDIR/$codeGestion/ -type f -name "$fileDocketedName" | cut -d '/' -f 4)
+ 	currentCompleteFileName=$( echo $completeFileName | cut -d '/' -f 4 )
+ 	sh glog.sh PROPRO "Archivo a procesar $currentCompleteFileName" INFO
+ 	sh mover.sh ./ACEPDIR/$codeGestion/$currentCompleteFileName ./PROCDIR/proc PROPRO
 done;
