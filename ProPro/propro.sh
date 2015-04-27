3de5bb17726e05aafddf852de0ff32b031e12d92
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-04-27 20:06:05 -0300

    obtengo el anio del nombre del archivo y saco el hardcode del codigo de gestion

--- ProPro/propro.sh
+++ ProPro/propro.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 countFiles="$(find ./ACEPDIR/Alfonsin -type f -printf x | wc -c)"
 sh glog.sh PROPRO "Inicio de propro \n \t\t\t Cantidad de archivos a procesar: $countFiles" INFO
+codGestion="Alfonsin"
 for filename in `ls ./ACEPDIR/Alfonsin/ | cut -d '_' -f 5 | sort -t - -k 3 -k 2 -k 1`; 
 do 
 	#protocolizar cada archivo
- 	currentFileName=$(find ./ACEPDIR/Alfonsin -type f -name "*$filename")
- 	resultado=$( echo $currentFileName | cut -d '/' -f 4 )
- 	sh glog.sh PROPRO "Archivo a procesar $resultado"
+ 	fileName=$(find ./ACEPDIR/$codGestion -type f -name "*$filename")
+ 	anioGestion=$(ls $fileName | cut -d '-' -f 3 | cut -d '.' -f 1)
+ 	currentFileName=$( echo $fileName | cut -d '/' -f 4 )
+ 	sh glog.sh PROPRO "Archivo a procesar $currentFileName" INFO
 done;
