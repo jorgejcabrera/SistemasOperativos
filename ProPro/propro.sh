ec1879d7c473dccfd0fb56c790f1d1604eb9d3d4
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-04-26 22:56:28 -0300

    loggeo el archivo que voy a protocolizar

--- ProPro/propro.sh
+++ ProPro/propro.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-input=/ACEPDIR/Alfonsin/*
 countFiles="$(find ./ACEPDIR/Alfonsin -type f -printf x | wc -c)"
-
- sh glog.sh PROPRO "Inicio de propro \n \t\t\t Cantidad de archivos a procesar: <$countFiles>" INFO
-
- for filename in `ls ./ACEPDIR/Alfonsin/ | cut -d '_' -f 5 | sort -t - -k 3 -k 2 -k 1`; 
- do 
- 	echo $filename; 
- done;
+sh glog.sh PROPRO "Inicio de propro \n \t\t\t Cantidad de archivos a procesar: $countFiles" INFO
+for filename in `ls ./ACEPDIR/Alfonsin/ | cut -d '_' -f 5 | sort -t - -k 3 -k 2 -k 1`; 
+do 
+	#protocolizar cada archivo
+ 	currentFileName=$(find ./ACEPDIR/Alfonsin -type f -name "*$filename")
+ 	resultado=$( echo $currentFileName | cut -d '/' -f 4 )
+ 	sh glog.sh PROPRO "Archivo a procesar $resultado"
+done;
