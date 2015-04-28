ca93b674b63e1c4c492866b10366de9be3b9178f
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-04-27 21:52:43 -0300

    refactor: obtengo el anio y el codigo de norma dentro del scope que va a protocolizar el documento

--- ProPro/propro.sh
+++ ProPro/propro.sh
@@ -5,14 +5,10 @@ countFiles="$(find ./ACEPDIR/$codeGestion -type f -printf x | wc -c)"
 
 sh glog.sh PROPRO "Inicio de propro \n \t\t\t Cantidad de archivos a procesar: $countFiles" INFO
 
-for completeFileName in `ls ./ACEPDIR/$codeGestion/ | cut -d '_' -f 5 | sort -t - -k 3 -k 2 -k 1`; 
-do 
+for completeFileName in `ls ./ACEPDIR/$codeGestion/ | cut -d '_' -f 5 | sort -t - -k 3 -k 2 -k 1`; do 
+ 	
  	completeFileName=$(find ./ACEPDIR/$codeGestion -type f -name "*$completeFileName" | cut -d '/' -f 4)
  	
- 	yearNorm=$(echo $completeFileName | cut -d '-' -f 3 | cut -d '.' -f 1)
-
- 	codeNorm=$(echo $completeFileName | cut -d '_' -f 2)
-
  	fileDocketedName="$yearNorm.$codeNorm"
 
  	fileAlreadyDocketed=$(find ./PROCDIR/proc/ -type f -name "$completeFileName" | cut -d '/' -f 4)
@@ -21,7 +17,11 @@ do
 
  		sh glog.sh PROPRO "Archivo a procesar $completeFileName" INFO
 
- 		sh mover.sh ./ACEPDIR/$codeGestion/$completeFileName ./PROCDIR/proc PROPRO
+ 		yearNorm=$(echo $completeFileName | cut -d '-' -f 3 | cut -d '.' -f 1)
+
+ 		codeNorm=$(echo $completeFileName | cut -d '_' -f 2)
+
+ 		#sh mover.sh ./ACEPDIR/$codeGestion/$completeFileName ./PROCDIR/proc PROPRO
 
  	else
 
