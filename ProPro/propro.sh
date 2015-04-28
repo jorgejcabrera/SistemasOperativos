cab3d048f67e6534f0732c1149333bbc4f704acd
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-04-27 23:04:08 -0300

    me fijo si la combinacion de codigo de norma y codigo de emisor es valida buscando en la tabla

--- ProPro/propro.sh
+++ ProPro/propro.sh
@@ -21,9 +21,21 @@ for completeFileName in `ls ./ACEPDIR/$codeGestion/ | cut -d '_' -f 5 | sort -t 
 
  		codeEmisor=$(echo  $completeFileName | cut -d '_' -f 3)
 
- 		fileDocketedName="$yearNorm.$codeNorm"
+ 		existCodeNormAndCodEmisorCombination=$(find ./MAEDIR/tab/nxe.tab -type f -print | xargs grep "$codeNorm;$codeEmisor")
 
- 		#sh mover.sh ./ACEPDIR/$codeGestion/$completeFileName ./PROCDIR/proc PROPRO
+ 		if [ ! -z $existCodeNormAndCodEmisorCombination ]; then
+
+ 			echo $existCodeNormAndCodEmisorCombination
+
+ 			fileDocketedName="$yearNorm.$codeNorm"
+
+ 			#sh mover.sh ./ACEPDIR/$codeGestion/$completeFileName ./PROCDIR/proc PROPRO
+
+ 		else
+
+ 			sh glog.sh PROPRO "Se rechaza el archivo por estar DUPLICADO" WAR
+ 		
+ 		fi
 
  	else
 
