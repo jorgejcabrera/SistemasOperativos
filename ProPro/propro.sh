8c5c0b5e27b49b8da6a960b520654b396c7b4c7f
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-04-30 23:22:19 -0300

    valido el codigo de firma

--- ProPro/propro.sh
+++ ProPro/propro.sh
@@ -56,6 +56,7 @@ for completeFileName in `ls ./ACEPDIR/$codeGestion/ | cut -d '_' -f 5 | sort -t 
  	
  	completeFileName=$(find ./ACEPDIR/$codeGestion -type f -name "*$completeFileName" | cut -d '/' -f 4)
  	fileAlreadyDocketed=$(find ./PROCDIR/proc/ -type f -name "$completeFileName" | cut -d '/' -f 4)								#me fijo si el archivo ya fue protocolizado
+ 	echo "1"
  	if [ -z $fileAlreadyDocketed ]																								#si el archivo no fue protocolizado, el find no nos retorna nada, y el string esta vacio
  	then
 
@@ -63,36 +64,58 @@ for completeFileName in `ls ./ACEPDIR/$codeGestion/ | cut -d '_' -f 5 | sort -t 
  		codeNorm=$(echo $completeFileName | cut -d '_' -f 2)																	#obtengo el codigo de norma del nombre del archivo
  		codeEmisor=$(echo  $completeFileName | cut -d '_' -f 3)																	#obtengo el codigo de emisor del nombre del archivo
  		existCodeNormAndCodEmisorCombination=$(find ./MAEDIR/tab/nxe.tab -type f -print | xargs grep "$codeNorm;$codeEmisor")	#me fijo si existe la combinacion de codigo de norma y emisor en la tabla nxe
+ 		echo "2"
  		if [ ! -z $existCodeNormAndCodEmisorCombination ]; then																	#si existe la combinacion, levanta la linea entera y el string no esto vacio
 
  			yearNorm=$(echo $completeFileName | cut -d '-' -f 3 | cut -d '.' -f 1)												#obtengo el año que esta en el nombre del archivo
  			fileDocketedName="$yearNorm.$codeNorm"																				#concateno el año de la norma con el codigo de norma para generar el nombre del archivo a protocolizar
  			date=$(echo $completeFileName | cut -d '_' -f 5 | cut -d '.' -f 1)
+ 			echo "3"
  			if [ $(validateDate $date) -eq 1 ]; then																			#me fijo si la fecha en el nombre del archivo a protocolizar es valida
  				
  				dateBegin=$(echo $RESULT_GEST | cut -d ';' -f 2)																#obtengo la fecha de comienzo de la gesiton
  				dateEnd=$(echo $RESULT_GEST | cut -d ';' -f 3)																	#obtengo la fecha de finalizacion de la gestion
+ 				echo "4"
  				if [ $(validateDateOnGest $dateBegin $dateEnd $date) -eq 1 ]; then												#me fijo si la fecha esta dentro del rango de la gestion												
 
  					typeGest=$(echo $RESULT_GEST | cut -d ';' -f 5)																#me fijo que tipo de gestion es, si es la actual, me devuelve 1 sino es un registro historico y me devuelve 0
+ 					echo "5"
+ 					
+ 					#PROCESANDO REGISTRO HISTORICO
  					if [ $typeGest -eq 0 ]; then																				#proceso un tipo de registro historico: tengo que validarlo
  						
  						resultNumberNorm=$(grep "\<$codeGestion.*\<$codeNorm" $MAE_COUNT_FILE)									#obtengo de la tabla de contadores por año de gestion la linea correspondiente al codigo de gestion y codigo de norma
+ 						echo "6"
  						if [ ! -z $resultNumberNorm ]; then																		#puede ocurrir que no se encuntre la linea que combina el codigo de norma y gestion y en ese caso el string estaria vacio
+ 							
  							numberNorm=$(echo $resultNumberNorm | cut -d ';' -f 6)												#parseo la linea para quedarme solo con el numero de norma
  							typeRegister=$(echo $RESULT_GEST | cut -d ';' -f 5)													#me fijo si es un archivo historico y corriente obteniendo el campo autoenumera de gestiones.mae
- 							codFirma=$(grep "^$codeEmisor" $MAE_EMISOR | cut -d ';' -f 3)										#obtengo el codigo de firma correspondiente al codigo de emisor en el nombre del archivo												
- 							codFirmaIntoFile=$(head -n 1 "ACEPDIR/$codeGestion/$completeFileName" | grep $codFirma | cut -d ';' -f 8) #busco el codigo de firma dentro del archivo
- 							echo $completeFileName
- 							echo $codFirmaIntoFile
-
+ 							echo "7"
  							if [ $numberNorm -lt 0 -a $typeRegister -eq 0 ]; then												#si el numero de norma es menor a 0 es invalido
+ 								
  								sh glog.sh PROPRO "El numero de norma $numberNorm es invalido. Se rechaza el archivo" ERR
  								#sh mover.sh ./ACEPDIR/$codeGestion/$completeFileName ./RECHDIR PROPRO
  								continue
  							#else
  																																#el numero de norma es mayor a 0 y se considera valido
  							fi
+ 						fi
+ 					#PROCESANDO ARCHIVO CORRIENTE
+ 					elif [ $typeGest -eq 1 ]; then
+ 						
+ 						codFirma=$(grep "^$codeEmisor" $MAE_EMISOR | cut -d ';' -f 3)										#obtengo el codigo de firma correspondiente al codigo de emisor en el nombre del archivo												
+ 						codFirmaIntoFile=$(head -n 1 "ACEPDIR/$codeGestion/$completeFileName" | grep $codFirma | cut -d ';' -f 8) #busco el codigo de firma dentro del archivo
+ 					 	echo "8"
+ 					 	if [ -z $codFirmaIntoFile ]; then
+ 					 		codFirmaIntoFile="valorPorDefecto"																#le clavo un valor por defecto a la variable para que no me moleste en el if cuando comparo el valor de ambas variables
+ 					 	fi
+ 					 	echo "9"
+ 						
+ 						if [ ! -z $codFirma ] && [ ! -z $codFirmaIntoFile ] && [ $codFirma != $codFirmaIntoFile ]; then		#el codigo de norma es invalido
+ 							echo "10"
+ 							sh glog.sh PROPRO "El numero de firma es invalido. Se rechaza el archivo"
+ 							#sh mover.sh ./ACEPDIR/$codeGestion/$completeFileName ./RECHDIR PROPRO
+ 							continue
  						fi
  					fi																				
  					#sh glog.sh PROPRO "La fecha $date está dentro del rango de la gestion $codeGestion" INFO
