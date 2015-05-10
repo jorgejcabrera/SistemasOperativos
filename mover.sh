a35ed874e7febed6619f4b955fc8aba5a20156ad
Nico Gago.- <nicogago.-@hotmail.com>
2015-05-10 17:38:39 -0300

    mover duplicados terminado

--- mover.sh
+++ mover.sh
@@ -8,47 +8,62 @@
 #   Archivo de Log del comando que la invoca (si corresponde)
 # Opciones y Parámetros
 #   Parámetro 1 (obligatorio): archivo origen
-#   Parámetro 2 (obligatorio): archivo destino
+#   Parámetro 2 (obligatorio): Directorio destino
 #   Parámetro 3 (opcional): comando que la invoca
 #
 
+DESTINO=$2
+PATHDESTINO="${2%/*}"
+PATHORIGEN="${1%/*}"
+ARORIGEN="${1##*/}"
+
 #No pueden ser menos de 2 ni mas de 3 parametros
 if [ $# -gt 3 -o $# -lt 2 ]; then
-	echo "Cantidad de parametros inválida"
+	sh glog.sh MOVER "Cantidad de parametros inválida" ERR
 	exit 1
 fi
 
-log="$3"
-rutadestino="${2%/*}"
-ardestino="${2##*/}"
-arorigen="${1##*/}"
-
 #Chequear que los primeros dos argumentos sean directorios validos
 if [ ! -f "$1" ];then #Chequeo si $1 es un archivo
-	if [ $# -eq 3 ]; then
-		sh glog.sh MOVER "El origen no existe" ERR
-	fi
+	sh glog.sh MOVER "El origen no existe" ERR
 	exit 2
 fi
 
-if [ ! -d "$rutadestino" ]; then #Chequeo si el directorio del destino existe
-        echo "destino no existe"
-	if [ $# -eq 3 ]; then
-		sh glog.sh MOVER "El directorio destino no existe" ERR
-	fi
+if [ ! -d "$PATHDESTINO" ]; then #Chequeo si el directorio del destino existe
+	sh glog.sh MOVER "El directorio destino no existe" ERR
 	exit 3
-else
-        rutadestino=$(echo "./$rutadestino")
-        ardestino="$1"
 fi
 
 #Chequear si origen y destino son distintos
-if [ "$1" = "$2" ]; then
+if [ "$PATHORIGEN" = "$DESTINO" ]; then
 		sh glog.sh MOVER "No se hizo nada porque el origen es igual al destino" ERR
+		exit 4
 fi
 
-mv "$1" "$2"
-if [ $# -eq 3 ]; then
-		sh glog.sh MOVER "Archivo $origen movido correctamente" INFO
+#Chequeo si hay duplicados
+if [ -d "$DESTINO" ]; then
+	DUPLICADO=$(find "$DESTINO" -maxdepth 1 -name "$ARORIGEN")
+	if [ -n "$DUPLICADO" ]; then # Si encuentro el archivo en destino
+		sh glog.sh MOVER "El archivo origen ya existe en destino, se mueve a $DUPDIR" WAR
+		mv "$1" "$DUPDIR" # Muevo el archivo a DUPDIR
+		NUMEROSECUENCIA=$( cat $CONFDIR/InsPro.conf | grep 'NUMEROSECUENCIA' | sed 's/NUMEROSECUENCIA=//' ) # 			Busco el N de secuencia en el archivo de conf
+		NUEVONOMBRE=$ARORIGEN"."$NUMEROSECUENCIA
+		mv "$DUPDIR/$ARORIGEN" "$DUPDIR/$NUEVONOMBRE"
+		NEWNUMSECUENCIA=$(expr $NUMEROSECUENCIA + 001)
+		if [ 10 -le "$NEWNUMSECUENCIA" ] && [ "$NEWNUMSECUENCIA" -le 99 ]; then
+			NEWNUMSECUENCIA="0$NEWNUMSECUENCIA"
+			echo "LALLA"
+		else
+			if [ "$NEWNUMSECUENCIA" -le 10 ]; then
+				NEWNUMSECUENCIA="00$NEWNUMSECUENCIA"
+			fi
+		fi					
+		$(sed -i 's/NUMEROSECUENCIA='$NUMEROSECUENCIA'/NUMEROSECUENCIA='$NEWNUMSECUENCIA'/' $CONFDIR/InsPro.conf)
+		exit 5
+	fi
 fi
+
+mv "$1" "$DESTINO"
+sh glog.sh MOVER "Archivo $1 movido correctamente" INFO
+
 exit 0
